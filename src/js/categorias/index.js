60dbece218989b83aa46e025c5cc766ab7084521
Adri25620 <adrian25martinez@gmail.com>
2025-05-21 17:30:15 -0600

    index.js guardar, limpiar, buscar y tabla

--- src/js/categorias/index.js
+++ src/js/categorias/index.js
@@ -1,8 +1,177 @@
 import { Dropdown } from "bootstrap";
 import Swal from "sweetalert2";
+import { validarFormulario } from '../funciones';
+import DataTable from "datatables.net-bs5";
+import { lenguaje } from "../lenguaje";
+import { data } from "jquery";
 
 
 const FormCategorias = document.getElementById('FormCategorias');
 const BtnGuardar = document.getElementById('BtnGuardar');
 const BtnModificar = document.getElementById('BtnModificar');
 const BtnLimpiar = document.getElementById('BtnLimpiar');
+
+
+const limpiarTodo = () => {
+
+    FormCategorias.reset();
+    BtnGuardar.classList.remove('d-none');
+    BtnModificar.classList.add('d-none');
+}
+
+
+const GuardarCategoria = async (event) => {
+
+    event.preventDefault();
+    BtnGuardar.disabled = true;
+
+    if (!validarFormulario(FormCategorias, ['cat_id'])) {
+        Swal.fire({
+            position: "center",
+            icon: "info",
+            title: "FORMULARIO INCOMPLETO",
+            text: "Debe de validar todos los campos",
+            showConfirmButton: true,
+        });
+        BtnGuardar.disabled = false;
+    }
+
+    const body = new FormData(FormCategorias);
+    const url = '/app01_AMB/categorias/guardarAPI';
+    const config = {
+        method: 'POST',
+        body
+    }
+
+    try {
+
+        const respuesta = await fetch(url, config);
+        const datos = await respuesta.json();
+
+        const { codigo, mensaje } = datos
+
+        if (codigo == 1) {
+
+            await Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Exito",
+                text: mensaje,
+                showConfirmButton: true,
+            });
+
+            limpiarTodo();
+            BuscarUsuarios();
+
+        } else {
+
+            await Swal.fire({
+                position: "center",
+                icon: "info",
+                title: "Error",
+                text: mensaje,
+                showConfirmButton: true,
+            });
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+    BtnGuardar.disabled = false;
+
+}
+
+
+const BuscarUsuarios = async () => {
+
+    const url = '/app01_AMB/categorias/buscarAPI';
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+
+        const respuesta = await fetch(url, config);
+        const datos = await respuesta.json();
+        const { codigo, mensaje, data } = datos
+
+        if (codigo == 1) {
+
+            await Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Exito",
+                text: mensaje,
+                showConfirmButton: true,
+            });
+
+            datatable.clear().draw();
+            datatable.rows.add(data).draw();
+        } else {
+
+            await Swal.fire({
+                position: "center",
+                icon: "info",
+                title: "Error",
+                text: mensaje,
+                showConfirmButton: true,
+            });
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const datatable = new DataTable('#TablaCategorias', {
+    dom: `
+        <"row mt-3 justify-content-between" 
+            <"col" l> 
+            <"col" B> 
+            <"col-3" f>
+        >
+        t
+        <"row mt-3 justify-content-between" 
+            <"col-md-3 d-flex align-items-center" i> 
+            <"col-md-8 d-flex justify-content-end" p>
+        >
+    `,
+    language: lenguaje,
+    data: [],
+    columns: [
+        {
+            title: 'No.',
+            data: 'us_id',
+            width: '%',
+            render: (data, type, row, meta) => meta.row + 1
+        },
+        { title: 'Nombre', data: 'cat_nombre' },
+        {
+            title: 'Acciones',
+            data: 'cat_id',
+            searchable: false,
+            orderable: false,
+            render: (data, type, row, meta) => {
+                return `
+                 <div class='d-flex justify-content-center'>
+                     <button class='btn btn-warning modificar mx-1' 
+                         data-id="${data}" 
+                         data-nombre="${row.cat_nombre}"    
+                         <i class='bi bi-pencil-square me-1'></i> Modificar
+                     </button>
+                     <button class='btn btn-danger eliminar mx-1' 
+                         data-id="${data}">
+                        <i class="bi bi-trash3 me-1"></i>Eliminar
+                     </button>
+                 </div>`;
+            }
+        }
+    ]
+});
+
+
+
+BuscarUsuarios();
+FormCategorias.addEventListener('submit', GuardarCategoria);
+BtnLimpiar.addEventListener('click', limpiarTodo);
